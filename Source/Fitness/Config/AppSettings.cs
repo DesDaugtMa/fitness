6030e25e322df0333ed01df0c482ff4915deb29d
FriedrAl <alexander.friedrich@asfinag.at>
2026-02-19 21:58:49 +0100

    google funktioniert

--- Source/Fitness/Config/AppSettings.cs
+++ Source/Fitness/Config/AppSettings.cs
@@ -3,12 +3,19 @@
     public class AppSettings
     {
         public ConnectionStrings? ConnectionStrings { get; set; }
+        public GoogleAuthSettings? GoogleAuthSettings { get; set; }
         public Version? Version { get; set; }
     }
 
     public class ConnectionStrings
     {
         public string? Default { get; set; }
+    }
+
+    public class GoogleAuthSettings
+    {
+        public string? ClientId { get; set; }
+        public string? ClientSecret { get; set; }
     }
 
     public class Version
