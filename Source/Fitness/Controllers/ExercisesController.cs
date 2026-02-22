16fc77354f8dada2d2d38d375f57a1a8e5b2f836
FriedrAl <alexander.friedrich@asfinag.at>
2026-02-22 17:07:18 +0100

    new

---
+++ Source/Fitness/Controllers/ExercisesController.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Fitness.DataAccess;
+using Fitness.DataAccess.Models;
+
+namespace Fitness.Controllers
+{
+    public class ExercisesController : Controller
+    {
+        private readonly FitnessDbContext _context;
+
+        public ExercisesController(FitnessDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Exercises
+        public async Task<IActionResult> Index()
+        {
+            var fitnessDbContext = _context.Exercises.Include(e => e.Creator).Include(e => e.Image);
+            return View(await fitnessDbContext.ToListAsync());
+        }
+
+        // GET: Exercises/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exercise = await _context.Exercises
+                .Include(e => e.Creator)
+                .Include(e => e.Image)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return View(exercise);
+        }
+
+        // GET: Exercises/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "DisplayName");
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id");
+            return View();
+        }
+
+        // POST: Exercises/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,CreatorId,Name,Description,ImageId,VideoUrl,IsPublic,CreatedAt")] Exercise exercise)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(exercise);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "DisplayName", exercise.CreatorId);
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id", exercise.ImageId);
+            return View(exercise);
+        }
+
+        // GET: Exercises/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exercise = await _context.Exercises.FindAsync(id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "DisplayName", exercise.CreatorId);
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id", exercise.ImageId);
+            return View(exercise);
+        }
+
+        // POST: Exercises/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,CreatorId,Name,Description,ImageId,VideoUrl,IsPublic,CreatedAt")] Exercise exercise)
+        {
+            if (id != exercise.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(exercise);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ExerciseExists(exercise.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "DisplayName", exercise.CreatorId);
+            ViewData["ImageId"] = new SelectList(_context.Images, "Id", "Id", exercise.ImageId);
+            return View(exercise);
+        }
+
+        // GET: Exercises/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var exercise = await _context.Exercises
+                .Include(e => e.Creator)
+                .Include(e => e.Image)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return View(exercise);
+        }
+
+        // POST: Exercises/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var exercise = await _context.Exercises.FindAsync(id);
+            if (exercise != null)
+            {
+                _context.Exercises.Remove(exercise);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ExerciseExists(int id)
+        {
+            return _context.Exercises.Any(e => e.Id == id);
+        }
+    }
+}
